93933adbab74eb75f4587d48fed7b44b4946f14b
yy <yy@yy.com>
2013-11-07 11:00:20 +0800

    first commit to cron script

---
+++ cron/clear-file.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+. /home/wyy/Dropbox/script/env_script.sh
+
+logfile=/var/log/cron/${0##*/}.log
+
+# need variable $pdir
+func_clear_dir()
+{
+  for dir in $( ls $dir2cleared | egrep "[0-9]{4}-[0-9]{2}-[0-9]{2}" )
+  do
+    if (( $(( $(date +%Y%m%d) - ${dir//-/} )) > 100 ))
+    then
+      rm -rf $dir2cleared/$dir && echo "rm -rf $dir2cleared/$dir"
+    fi
+  done
+}
+
+main_func()
+{
+  func_tmstamp
+  dir2cleared=/home/wyy/1cloud_data
+  func_clear_dir
+  unset dir2cleared
+}
+
+
+if [[ -n $TERM ]]
+then
+  main_func 2>&1 | tee -a $logfile
+else
+  main_func >> $logfile 2>&1
+fi
---
+++ cron/github-push.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+. /home/wyy/bash_functions/script/func-tmstamp.sh
+. /home/wyy/bash_functions/system/func-gitall.sh
+. /home/wyy/bash_functions/script/func-log22file.sh
+
+logfile=/var/log/cron/${0##*/}.log
+
+dir=( "/home/wyy/vim_conf"
+      "/home/wyy/conf_file"
+      "/home/wyy/bash_functions"
+      "/var/www/cgi-bin/sync_mc_or_music"
+      )
+
+main_func()
+{
+  func_tmstamp
+
+  for i in "${dir[@]}"
+  do
+    if cd $i
+    then
+      echo -e "\n==$(pwd)==================="
+      func_gitall 'crontab'
+    fi
+  done
+}
+
+
+func_log22file
+
