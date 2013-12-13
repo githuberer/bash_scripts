cb7213c86601c8793ed9de7de2863847290d4181
yy <yy@yy.com>
2013-12-13 11:39:09 +0800

    newfile

---
+++ cron/clear-log-by-size.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+scriptname=$(basename $0)
+logfile=/u/logs/${scriptname/.sh/.log}
+files="find /usr/local/nginx/logs -name '*.log' -size  +100M |paste -s -d ' '"
+
+main_func()
+{
+  for fff in $files
+  do
+    if tail -v -c 100000K $fff > $fff.old
+    then
+      echo -e "\n\n
+      ==========================================
+      date +%Y-%m-%d_%H:%M:%S
+      ==========================================
+      Split file and zero it
+      original:  $fff
+      old backup: $fff.old
+      \n\n\n" |tee $fff
+    fi
+  done
+}  
+
+
+[[ -d $logfile ]] || mkdir -p $logfile
+
+if [[ -n $TERM ]]
+then
+  main_func 2>&1 | tee -a $logfile
+else
+  main_func >> $logfile 2>&1
+fi
+
+
