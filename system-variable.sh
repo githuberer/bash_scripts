c6d90459fc52cffdf149a0587011fe16c58ff6ba
yy <yy@yy.com>
2014-03-26 11:56:45 +0800

    NULL

--- system-variable.sh
+++ system-variable.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 #This is the environment for interactive bash
 #shopt -s expand_aliases
-#
+
 export TERM="xterm-256color"
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/wyy/Dropbox/script
 export MAIL=/home/wyy/mail/inbox
 export EDITOR="vim"
 export VISUAL="vim"
 export CDPATH=$CDPATH:/home/wyy:/var/www
+
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US:en"
+export LC_CTYPE=en_US.UTF-8
+export LANG=en_US.UTF-8
+#LANG="en_us 8859 1"
+
 
 Script=/home/wyy/Dropbox/script
 
@@ -17,10 +25,6 @@ curr_t=$(date +%H:%M)
 curr_dt=$(date +%Y-%m-%d_%H:%M)
 curr_dts=$(date +%Y-%m-%d_%H:%M:%S)
 
-LC_ALL="en_US.UTF-8"
-LANG="en_US.UTF-8"
-LANGUAGE="en_US:en"
-#LANG="en_us 8859 1"
 
 alias ll='ls -l'
 alias rm='rm -i'
@@ -28,5 +32,4 @@ alias cp='cp -i'
 alias mv='mv -i'
 
 
-#. /usr/local/rvm/scripts/rvm
 . /etc/profile.d/rvm.sh
