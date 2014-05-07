d4e787c5c47ef75e4834c8770267ac414fe565d5
WangYanying <wangyanying1@gmail.com>
2014-05-07 15:45:28 +0800

    zshrc code merge to sys-var.sh & useless code remove

--- system-variable.sh
+++ system-variable.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-#This is the environment for interactive bash
+#This is the environment of interactive shell
 #shopt -s expand_aliases
 
 export TERM="xterm-256color"
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/wyy/Dropbox/script:/home/wyy/ruby_scripts/bin
+export PATH=$PATH:/home/wyy/Dropbox/script:/home/wyy/ruby_scripts/bin
 export MAIL=/home/wyy/mail/inbox
 export EDITOR="vim"
 export VISUAL="vim"
-export CDPATH=$CDPATH:/home/wyy:/var/www
+export CDPATH=$CDPATH:/home/wyy
 
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
@@ -15,7 +15,6 @@ export LANGUAGE="en_US:en"
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 #LANG="en_us 8859 1"
-
 
 Script=/home/wyy/Dropbox/script
 
@@ -25,11 +24,36 @@ curr_t=$(date +%H:%M)
 curr_dt=$(date +%Y-%m-%d_%H:%M)
 curr_dts=$(date +%Y-%m-%d_%H:%M:%S)
 
-
 alias ll='ls -l'
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
+
+# colorized man pages with less Command
+man() {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
+}
+
+# colorized ri
+alias ri='ri -f ansi'
+
+# for emacs prompt
+if [[ -n ${INSIDE_EMACS} ]]; then
+    # This shell runs inside an Emacs *shell*/*term* buffer.
+    prompt walters
+    unsetopt zle
+fi
+
+# Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
 
 
 . /etc/profile.d/rvm.sh
