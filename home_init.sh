5fb8ac867c8667fba045e7e37a48a5d7d82fc350
WangYanying <wangyanying1@gmail.com>
2014-06-09 10:39:25 +0800

    NULL

---
+++ home_init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+rm -rf 下载
+ln -sf /media/data/download ~/下载
+
+rm -rf 音乐
+ln -sf /media/data/music ~/音乐
+
+rm -rf 文档
+ln -sf /media/data/document ~/文档
+
+rm -rf 图片
+ln -sf /media/data/picture ~/图片
+
+rm -rf 视频
+ln -sf /media/data/video ~/视频
