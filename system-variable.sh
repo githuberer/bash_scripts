100cdc9edf341a4e34e324d716ad3d685572b096
WangYanying <wangyanying1@gmail.com>
2014-06-12 16:31:12 +0800

    NULL

--- system-variable.sh
+++ system-variable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#This is the environment of interactive shell
-#shopt -s expand_aliases
+
+### variables ###################################
 
 export TERM="xterm-256color"
 export PATH=$PATH:$HOME/Dropbox/script:/home/wyy/ruby_scripts/bin
@@ -8,15 +8,34 @@ export MAIL=$HOME/mail/inbox
 export EDITOR="vim"
 export VISUAL="vim"
 export CDPATH=$CDPATH:$HOME
+export HISTTIMEFORMAT='%F %T  '
 
 #export LC_ALL="en_US.UTF-8"
 #export LANG="en_US.UTF-8"
 #export LANGUAGE="en_US:en"
 #export LC_CTYPE=en_US.UTF-8
 #export LANG=en_US.UTF-8
-#LANG="en_us 8859 1"
 
-Script=$HOME/Dropbox/script
+SCRIPT=$HOME/Dropbox/script
+
+
+### alias #######################################
+
+# colorized ri
+alias ri='ri -f ansi'
+
+#sudo vim
+alias svim='sudo vim'
+
+#sudo last command
+alias slcmd='sudo ${history[${#history}]}'
+
+# lvm
+alias lvs='sudo lvs'
+alias vgs='sudo vgs'
+alias lvcreate='sudo lvcreate'
+alias lvremove='sudo lvremove'
+alias lvresize='sudo lvresize'
 
 # inactive rm cp mv
 alias ll='ls -l'
@@ -24,30 +43,25 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-#sudo vim
-alias svim='sudo vim'
-
 #debian aptitude
 alias asearch='sudo aptitude search'
 alias ashow='sudo aptitude show'
 alias aversions='sudo aptitude versions'
-
 alias ainstall='sudo aptitude install'
-alias ainstall='sudo aptitude remove'
+alias aremove='sudo aptitude remove'
 alias apurge='sudo aptitude purge'
 alias aremove='sudo aptitude remove'
-
 alias aupdate='sudo aptitude update'
 alias aupgrade='sudo aptitude upgrade'
 
 #debian dpkg
-alias dpkgl='sudo dpkg -l'
-alias dpkgi='sudo dpkg -i'
-alias dpkgr='sudo dpkg -r'
+alias dplist='sudo dpkg -l'
+alias dpinstall='sudo dpkg -i'
+alias dpremove='sudo dpkg -r'
 
-#git
-alias gits='git status'
 
+
+### others ####################################
 # colorized man pages with less Command
 man() {
   env \
@@ -61,8 +75,6 @@ man() {
     man "$@"
 }
 
-# colorized ri
-alias ri='ri -f ansi'
 
 # for emacs prompt
 if [[ -n ${INSIDE_EMACS} ]]; then
