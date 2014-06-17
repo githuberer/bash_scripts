96654a04fe4c65f606bab339131edf61e8a04d44
WangYanying <wangyanying1@gmail.com>
2014-06-17 17:32:03 +0800

    NULL

--- shellconf.sh
+++ shellconf.sh
@@ -38,10 +38,10 @@
 #
 
 ### zsh ###################################
-export ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="robbyrussell"
-plugins=(git rails)
-source $ZSH/oh-my-zsh.sh
+#export ZSH=$HOME/.oh-my-zsh
+#ZSH_THEME="robbyrussell"
+#plugins=(git rails)
+#source $ZSH/oh-my-zsh.sh
 
 ### variables ###################################
 
