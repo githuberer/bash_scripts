f44a4738736f125e71ea94f8e3d22b12c00573c3
WangYanying <wangyanying1@gmail.com>
2014-06-17 09:32:22 +0800

    NULL

--- tools/environment_init_cent6.sh
+++ tools/environment_init_cent6.sh
@@ -106,7 +106,7 @@ set clipboard=unnamedplus     "murge vim clipboard to system clipboard
 "-----"code indent
 set autoindent              
 set expandtab
-"set tabstop=8 shiftwidth=4 softtabstop=4
+set tabstop=8 shiftwidth=4 softtabstop=4
 au FileType python set tabstop=8 shiftwidth=4 softtabstop=4
 au FileType sh set tabstop=8 shiftwidth=4 softtabstop=4
 au FileType ruby set tabstop=4 shiftwidth=2 softtabstop=2
