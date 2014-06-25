567318c2ae7fd107f3a09022943beaf6c3f08070
WangYanying <wangyanying1@gmail.com>
2014-06-25 17:41:39 +0800

    NULL

--- monit/script/nettraffic.sh
+++ monit/script/nettraffic.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-t=10         # seconds
-limitn=5     # MiB/s
+t=5           # seconds
+limitn=10     # Mbit/s
 
 __FILE__=$(readlink -f $0)
 log=${__FILE__/.sh/.log}
 
-vnstat -tr $t -ru &> $log
+vnstat -tr $t &> $log
 
 n=$(grep 'tx' $log | awk '{print $2}' | awk -F. '{print $1}')
 unit=$(grep 'tx' $log | awk '{print $3}')
 
 
-if ( [[ $unit =~ "MiB/s" ]] && (( $n > $limitn )) ) || [[ $unit =~ "GiB/s" ]]
+if ( [[ $unit =~ "Mbit/s" ]] && (( $n > $limitn )) ) || [[ $unit =~ "Gbit/s" ]]
 then
     echo -e "\n\n Net traffic average(tx|$t seconds) [current $n $unit] matches resource limit [$limitn $unit] \n\n Details:"
     grep -E "(rx|tx)"  $log
