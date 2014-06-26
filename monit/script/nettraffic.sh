f492b88190ee3f56cf90e49820eadea64b0faa5c
WangYanying <wangyanying1@gmail.com>
2014-06-26 18:45:59 +0800

    NULL

--- monit/script/nettraffic.sh
+++ monit/script/nettraffic.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
 t=5           # seconds
-limitn=50     # Mbit/s
+limitn=0     # Mbit/s
 
 __FILE__=$(readlink -f $0)
 log=${__FILE__/.sh/.log}
+netstat=${__FILE__/.sh/.netstat}
+
+
+if [[ $1 == "netstat" ]]
+then
+    netstat -anp --inet > $netstat
+    exit 0
+fi
+
+if [[ -n $1 ]] && [[ $1 != "update" ]]
+then
+    echo -e "\n  Valid param should be: netstat \n"
+    exit 0
+fi
+
+
 
 vnstat -tr $t &> $log
 
@@ -12,11 +28,13 @@ n=$(grep 'tx' $log | awk '{print $2}' | awk -F. '{print $1}')
 unit=$(grep 'tx' $log | awk '{print $3}')
 
 
-if ( [[ $unit =~ "Mbit/s" ]] && (( $n > $limitn )) ) || [[ $unit =~ "Gbit/s" ]]
+if ( [[ $unit =~ "kbit/s" ]] && (( $n > $limitn )) ) || [[ $unit =~ "Gbit/s" ]]
 then
-    echo -e "\n\n Net traffic average(tx|$t seconds) current [$n $unit] matches resource limit [$limitn $unit] \n\n Details:"
+    echo -e "\n Net traffic average(tx|$t seconds) current [$n $unit] matches resource limit [$limitn $unit] \n Details:"
     grep -E "(rx|tx)"  $log
-    echo -e "\n See More: vnstat -l || iftop -PB || nethogs \n"
-    echo -e "\n Check Connections: /tmp/netstat.log \n"
+    echo -e "\n Check Connections: $netstat "
+    echo -e "\n See More: vnstat -l || iftop -PB || nethogs "
     exit 1
 fi
+
+
