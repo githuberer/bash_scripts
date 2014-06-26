2b7208313a1c3817327dab2bd2d6976974c15203
WangYanying <wangyanying1@gmail.com>
2014-06-26 11:11:56 +0800

    NULL

--- monit/script/nettraffic.sh
+++ monit/script/nettraffic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 t=5           # seconds
-limitn=80     # Mbit/s
+limitn=50     # Mbit/s
 
 __FILE__=$(readlink -f $0)
 log=${__FILE__/.sh/.log}
@@ -16,8 +16,7 @@ if ( [[ $unit =~ "Mbit/s" ]] && (( $n > $limitn )) ) || [[ $unit =~ "Gbit/s" ]]
 then
     echo -e "\n\n Net traffic average(tx|$t seconds) current [$n $unit] matches resource limit [$limitn $unit] \n\n Details:"
     grep -E "(rx|tx)"  $log
-    echo -e "\n See More: vnstat -l -ru || iftop -PB || nethogs \n"
+    echo -e "\n See More: vnstat -l || iftop -PB || nethogs \n"
     echo -e "\n Check Connections: /tmp/netstat.log \n"
     exit 1
 fi
-
