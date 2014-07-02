4cc9245a9a0f7ade2888a68039892e0afda63a0e
WangYanying <wangyanying1@gmail.com>
2014-07-02 11:22:56 +0800

    init.d => dircontent

--- monit/script/dircontent.sh
+++ monit/script/dircontent.sh
@@ -2,14 +2,14 @@
 
 __FILE__=$(readlink -f $0)
 
-
+dir=/etc/init.d
 ago=${__FILE__/.sh/.ago}
 now=${__FILE__/.sh/.now}
 
 
 if [[ $1 == "update" ]]
 then
-    ls -a /etc/init.d |sort > $ago
+    ls -a $dir |sort > $ago
     exit 0
 fi
 
@@ -20,17 +20,17 @@ then
 fi
 
 
-ls -a /etc/init.d |sort > $now
+ls -a $dir |sort > $now
 
 news=$(comm -23 $now $ago)
 
 if [[ -n $news ]]
 then
-    echo -e "\n\n New sysinit script detected in \"/etc/init.d\". "
+    echo -e "\n\n New sysinit script detected in \"$dir\". "
     echo -e "\n\n Details:"
     for n in $news
     do
-        ls -l /etc/init.d/$n
+        ls -l $dir/$n
     done
     echo -e "\n\n More: Trust in the new sysinit and update snapshot with \"$__FILE__ update\". \n"
     exit 1
