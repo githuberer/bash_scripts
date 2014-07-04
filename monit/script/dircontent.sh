aa73ab9bf1e981b10cdd914265ba12d01083c2b1
WangYanying <wangyanying1@gmail.com>
2014-07-04 11:37:35 +0800

    NULL

--- monit/script/dircontent.sh
+++ monit/script/dircontent.sh
@@ -33,5 +33,14 @@ then
         ls -l $dir/$n
     done
     echo -e "\n\n More: Trust in the new sysinit and update snapshot with \"$__FILE__ update\". \n"
+
+    echo -e "\n\n lsof log:  ${__FILE__/.sh/.lsof} \n"
+    for n in $news
+    do
+        pids=$(ps -ef | grep $n | grep -v "grep" | awk '{print $2}' | paste -d ",")
+        echo -e "\n\n lsof -p $pids \n"
+        lsof -p $pids
+    done &> ${__FILE__/.sh/.lsof}
+
     exit 1
 fi
