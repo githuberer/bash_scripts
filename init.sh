d0a45c85c30c1f5d4b227fae838dc81b8262fa1c
WangYanying <wangyanying1@gmail.com>
2014-10-01 01:52:31 +0800

    nil

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ gem install wirble
 
 ln -sf $HOME/bash_scripts/irbrc $HOME/.irbrc
 
+
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
 test -f $HOME/.zshrc && ( echo ". $(readlink -f shellinit.sh)" >> $HOME/.zshrc )
-test -f $HOME/.bashrc && ( echo ". $(readlink -f shellinit.sh)" >> $HOME/.bashrc )
 
