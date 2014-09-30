28b2f786809ef5034d03b36e565f7c6403408684
WangYanying <wangyanying1@gmail.com>
2014-10-01 02:17:14 +0800

    nil

--- shellinit.sh
+++ shellinit.sh
@@ -29,6 +29,9 @@ SCRIPT=$HOME/Dropbox/script
 alias gin='gem install --http-proxy --source http://rubygems.org'
 alias gintao='gem install --http-proxy --source http://ruby.taobao.org'
 
+alias gsch='gem search --http-proxy --source http://rubygems.org'
+alias gschtao='gem search --http-proxy --source http://ruby.taobao.org'
+
 alias gup='gem update --http-proxy --source http://rubygems.org'
 alias guptao='gem update --http-proxy --source http://ruby.taobao.org'
 
