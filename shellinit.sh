3ccfcaa223fb31e91549b0652bc667a629f37e23
WangYanying <wangyanying1@gmail.com>
2014-10-01 00:40:34 +0800

    nil

--- shellinit.sh
+++ shellinit.sh
@@ -3,8 +3,7 @@
 ### zsh ########################################
 export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="robbyrussell"
-plugins=(git)
-#plugins=(git rails)
+plugins=(git rails)
 source $ZSH/oh-my-zsh.sh
 
 
