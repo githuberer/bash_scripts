8fc167188f18f99e18f212c1c7b88c2680ddf14f
YanyingWang <yanyingwangwang@gmail.com>
2014-11-20 10:51:33 +0800

    chan colorize man with oh_my_zsh

--- shellinit.sh
+++ shellinit.sh
@@ -3,7 +3,7 @@
 ### zsh ########################################
 export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="robbyrussell"
-plugins=(git rails)
+plugins=(git rails colored-man)
 source $ZSH/oh-my-zsh.sh
 
 
@@ -148,17 +148,17 @@ alias slcmd='sudo ${history[${#history}]}'
 
 ### others ####################################
 # colorized man pages with less Command
-man() {
-  env \
-    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-    LESS_TERMCAP_md=$(printf "\e[1;31m") \
-    LESS_TERMCAP_me=$(printf "\e[0m") \
-    LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-    LESS_TERMCAP_ue=$(printf "\e[0m") \
-    LESS_TERMCAP_us=$(printf "\e[1;32m") \
-    man "$@"
-}
+#man() {
+  #env \
+    #LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    #LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    #LESS_TERMCAP_me=$(printf "\e[0m") \
+    #LESS_TERMCAP_se=$(printf "\e[0m") \
+    #LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    #LESS_TERMCAP_ue=$(printf "\e[0m") \
+    #LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    #man "$@"
+#}
 
 __FILE__=$(readlink -f $0)
 VIRHOME=$(dirname $__FILE__)
