6b0ecd4bf38af4f8c85f2a239a8edb088af36550
YanyingWang <yanyingwangwang@gmail.com>
2014-12-07 02:50:00 +0800

    zsh-syntax-highlighting

--- shellinit.sh
+++ shellinit.sh
@@ -3,7 +3,7 @@
 ### zsh ########################################
 export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="robbyrussell"
-plugins=(git rails colored-man)
+plugins=(git rails colored-man zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
 
 
